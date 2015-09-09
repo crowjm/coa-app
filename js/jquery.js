2752ae82ba051dfdabd0e804d72de502c93d55aa
Jesse Crow <jessemcrow@gmail.com>
2015-09-09 00:11:52 -0500

    added icons, wordmark, and basic sass/jquery

---
+++ js/jquery.js
@@ -0,0 +1,31 @@
+//show and hide content under headers on main page
+
+$("#about-header").click (
+	function(){
+		$("#about-content").toggle();
+	}
+);
+
+$("#why-header").click (
+	function(){
+		$(".why-content").toggle();
+	}
+);
+
+$("#how-header").click (
+	function(){
+		$("#how-content").toggle();
+	}
+);
+
+$("#govt-header").click (
+	function(){
+		$("#govt-content").toggle();
+	}
+);
+
+$("#map-header").click (
+	function(){
+		$("#map-content").toggle();
+	}
+);
